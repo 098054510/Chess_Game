e79d7cfae283b73daeaa684b25e008a05e9b6319
Eduardo de Souza <eduardohsfreitas86@gmail.com>
2018-10-26 08:30:09 -0300

    Insertion of Chess_Game in github

---
+++ Chess_Game/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Chess_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Changes to get start the project");
+            Console.ReadLine();
+        }
+    }
+}
