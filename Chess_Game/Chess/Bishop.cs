f8eaef94dcfc6fd7fbc9c3d47c49d6284ec24d79
Eduardo de Souza <eduardohsfreitas86@gmail.com>
2018-10-26 11:38:53 -0300

    Have errors to replace

---
+++ Chess_Game/Chess/Bishop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BattleField;
+
+namespace Chess
+{
+    class Bishop : Piece
+    {
+        public Bishop(BattleField Bat, Color color) : base(Bat, color)
+        {
+        }
+
+
+    }
+}
