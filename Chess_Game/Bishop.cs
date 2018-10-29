3fd9489e43d490a19edfeb1917d3b4bb872d9e3c
Eduardo de Souza <eduardohsfreitas86@gmail.com>
2018-10-29 08:03:53 -0300

    Created Bishop class with error in PossibleMovement()

--- Chess_Game/Bishop.cs
+++ Chess_Game/Bishop.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Chess_Game.BattleField;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -10,6 +11,72 @@ namespace Chess
         {
         }
 
+        public override string ToString()
+        {
+            return "B";
+        }
 
+        private bool CanMove(Position position)
+        {
+            Piece piece = Bat.piece(position);
+            return piece == null || piece.color != color;
+        }
+
+        public override bool[,] PossibleMovement()
+        {
+            bool[,] mat = new bool[Bat.Line, Bat.Collum];
+
+            Position position = new Position(0, 0);
+
+            //NO
+            position.SetValues(position.Line - 1, position.Collum - 1);
+            while (Bat.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Collum] = true;
+                if (Bat.piece(position) != null && Bat.piece(position).color != color)
+                {
+                    break;
+                }
+                position.SetValues(position.Line - 1, position.Collum - 1);
+            }
+
+            //NE
+            position.SetValues(position.Line - 1, position.Collum + 1);
+            while (Bat.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Collum] = true;
+                if (Bat.piece(position) != null && Bat.piece(position).color != color)
+                {
+                    break;
+                }
+                position.SetValues(position.Line - 1, position.Collum + 1);
+            }
+
+            //SE
+            position.SetValues(position.Line + 1, position.Collum + 1);
+            while (Bat.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Collum] = true;
+                if (Bat.piece(position) != null && Bat.piece(position).color != color)
+                {
+                    break;
+                }
+                position.SetValues(position.Line + 1, position.Collum + 1);
+            }
+
+            //SO
+            position.SetValues(position.Line + 1, position.Collum - 1);
+            while (Bat.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Collum] = true;
+                if (Bat.piece(position) != null && Bat.piece(position).color != color)
+                {
+                    break;
+                }
+                position.SetValues(position.Line + 1, position.Collum - 1);
+            }
+
+            return mat;
+        }
     }
 }
