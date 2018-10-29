ecb6588504b2386f4d447373061343ffb7d40e17
Eduardo de Souza <eduardohsfreitas86@gmail.com>
2018-10-29 08:59:19 -0300

    Insertion of Horse.cs

---
+++ Chess_Game/Horse.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Chess_Game.BattleField;
+
+namespace Chess_Game
+{
+    class Horse : Piece
+    {
+        public Horse(BattleField Bat, Color color) : base(Bat, color)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "H";
+        }
+
+        private bool CanMove(Position position)
+        {
+            Piece piece = Bat.piece(position);
+            return piece == null || piece.color != color;
+        }
+
+        public override bool[,] PossiblesMovements()
+        {
+            bool[,] mat = new bool[Bat.Line, Bat.Collum];
+
+            Position position = new Position(0, 0);
+
+            position.SetValues(position.Line - 1, position.Collum - 2);
+            if (Bat.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Collum] = true;
+            }
+
+            position.SetValues(position.Line - 2, position.Collum - 1);
+            if (Bat.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Collum] = true;
+            }
+
+            position.SetValues(position.Line - 2, position.Collum + 1);
+            if (Bat.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Collum] = true;
+            }
+
+            position.SetValues(position.Line - 1, position.Collum + 2);
+            if (Bat.ValidPosition(position) && CanMove(position){
+                mat[position.Line, position.Collum] = true;
+            }
+
+            position.SetValues(position.Line + 1, position.Collum + 2);
+            if (Bat.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Collum] = true;
+            }
+
+            position.SetValues(position.Line + 2, position.Collum + 1);
+            if (Bat.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Collum] = true;
+            }
+
+            position.SetValues(position.Line + 2, position.Collum - 1);
+            if (Bat.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Collum] = true;
+            }
+
+            position.SetValues(position.Line + 1, position.Collum - 2);
+            if (Bat.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Collum] = true;
+            }
+
+            return mat;
+        }
+    }
+}
