a84a2759a269d712ae5cc5ebd5fa63c0c229c2d7
Eduardo de Souza <eduardohsfreitas86@gmail.com>
2018-10-30 07:36:49 -0300

    Classes King.cs and Tower.cs Finished

--- Chess_Game/King.cs
+++ Chess_Game/King.cs
@@ -25,7 +25,101 @@ namespace Chess
         private bool TowertoRoquetest(Position position)
         {
             Piece piece = Bat.piece(position);
-            return piece != null && piece is 
+            return piece != null && piece is Tower && piece.color == color && piece.Movement == 0;
+        }
+
+        public override bool[,] PossiblesMovements()
+        {
+            bool[,] mat = new bool[Bat.Line, Bat.Collum];
+
+            Position position = new Position(0, 0);
+
+            //Above
+            position.SetValues(position.Line - 1, position.Collum);
+            if (Bat.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Collum] = true;
+            }
+
+            //NE
+            position.SetValues(position.Line - 1, position.Collum + 1);
+            if (Bat.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Collum] = true;
+            }
+
+            //Right
+            position.SetValues(position.Line, position.Collum + 1);
+            if (Bat.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Collum] = true;
+            }
+
+            //SE
+            position.SetValues(position.Line + 1, position.Collum + 1);
+            if (Bat.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Collum] = true;
+            }
+
+            //Below
+            position.SetValues(position.Line + 1, position.Collum);
+            if (Bat.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Collum] = true;
+            }
+
+            //SO
+            position.SetValues(position.Line + 1, position.Collum - 1);
+            if (Bat.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Collum] = true;
+            }
+
+            //Left
+            position.SetValues(position.Line, position.Collum - 1);
+            if (Bat.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Collum] = true;
+            }
+
+            //NO
+            position.SetValues(position.Line - 1, position.Collum - 1);
+            if (Bat.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Collum] = true;
+            }
+
+            //Especial Play Roque
+            if (Movement==0 && !chessParty.CheckMate)
+            {
+                //Especial Play Little Roque
+                Position position1 = new Position(position.Line, position.Collum + 3);
+                if (TowertoRoquetest(position1))
+                {
+                    Position p1 = new Position(position.Line, position.Collum + 1);
+                    Position p2 = new Position(position.Line, position.Collum + 2);
+                    if (Bat.piece(p1)==null && Bat.piece(p2) == null)
+                    {
+                        mat[position.Line, position.Collum + 2] = true;
+                    }
+                }
+
+                //Especial Play Big Roque
+                Position position2 = new Position(position.Line, position.Collum - 4);
+                if (TowertoRoquetest(position2))
+                {
+                    Position p1 = new Position(position.Line, position.Collum - 1);
+                    Position p2 = new Position(position.Line, position.Collum - 2);
+                    Position p3 = new Position(position.Line, position.Collum - 3);
+                    if (Bat.piece(p1) == null && Bat.piece(p2) == null && Bat.piece(p3) == null)
+                    {
+                        mat[position.Line, position.Collum - 2] = true;
+                    }
+                }
+            }
+
+            return mat;
         }
     }
 }
--- Chess_Game/Tower.cs
+++ Chess_Game/Tower.cs
@@ -26,6 +26,54 @@ namespace Chess
             Position position = new Position(0, 0);
 
             //Above
+            position.SetValues(position.Line - 1, position.Collum);
+            while (Bat.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Collum] = true;
+                if (Bat.piece(position) != null && Bat.piece(position).color != color)
+                {
+                    break;
+                }
+                position.Line = position.Line - 1;
+            }
+
+            //Below
+            position.SetValues(position.Line + 1, position.Collum);
+            while (Bat.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Collum] = true;
+                if (Bat.piece(position) != null && Bat.piece(position).color != color)
+                {
+                    break;
+                }
+                position.Line = position.Line + 1;
+            }
+
+            //Right
+            position.SetValues(position.Line, position.Collum + 1);
+            while (Bat.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Collum] = true;
+                if (Bat.piece(position) != null && Bat.piece(position). color != color)
+                {
+                    break;
+                }
+                position.Collum = position.Collum + 1;
+            }
+
+            //Left
+            position.SetValues(position.Line, position.Collum - 1);
+            while (Bat.ValidPosition(position) && CanMove(position))
+            {
+                mat[position.Line, position.Collum] = true;
+                if (Bat.piece(position) != null && Bat.piece(position).color != color)
+                {
+                    break;
+                }
+                position.Collum = position.Collum - 1;
+            }
+
+            return mat;
         }
     }
 }
